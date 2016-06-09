cd923fa73d592ff96bdc4555b3278750c3307a80
ViktorFP <VikPr_85@mail.ru>
2016-06-09 13:50:49 +0300

    first test

--- Struts/src/test/ReservationTest.java
+++ Struts/src/test/ReservationTest.java
@@ -1,33 +1,63 @@
 package test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
-import by.epamlab.beans.reservations.EjbReservation;
+import by.epamlab.beans.User;
+import by.epamlab.beans.ifaces.IReservationDAO;
 import by.epamlab.beans.reservations.ResComponent;
-import by.epamlab.beans.reservations.Reservation;
+import by.epamlab.impl.EjbUserImpl;
+import by.epamlab.impl.ReservationImplXml;
+import by.epamlab.impl.ReservationImplXmlEjb;
 
 public class ReservationTest {
+
+	private IReservationDAO reservationDAO;
+
 	@Test
-	public void testGetResComponents() throws Exception {
+	public void testGetResComponents() throws IOException, SAXException {
 		// test with started servers
 
 		// Arrange
-		Reservation reservation = new Reservation();
+		reservationDAO = spy(new ReservationImplXml());
 		// Act
-		List<ResComponent> components = reservation.getResComponents("", "");
+		List<ResComponent> components = getUser().getReservation().getResComponents("", "");
 		// Assert
 		assertNotNull(components);
-
-		// ------------
+		// -----
 		// Arrange
-		reservation = new EjbReservation();
+		reservationDAO = spy(new ReservationImplXmlEjb());
 		// Act
-		components = reservation.getResComponents("", "");
+		components = getUser().getReservation().getResComponents("", "");
 		// Assert
 		assertNotNull(components);
+	}
+
+	private User getUser() throws IOException, SAXException {
+		return spy(new User("user", reservationDAO.getReservation("0004257753")));
+	}
+
+	@Test
+	public void testgetReservation_not_null() {
+		// Arrange
+		EjbUserImpl ejbUserImpl = mock(EjbUserImpl.class);
+		when(ejbUserImpl.getUser(anyString(), anyString())).thenCallRealMethod();
+		// Act
+		User user = ejbUserImpl.getUser("user", "user");
+		// Assert
+		if (user != null) {
+			assertThat(user.getReservation(), notNullValue());
+		}
 	}
 }
